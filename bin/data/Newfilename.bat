cbd67260a81a80694bb4b0f7b2b47526093bec41
karinakorsgaard <kkorsgaardjensen@gmail.com>
2018-03-17 05:14:29 -0700

    sumatraPdf + print for windows

--- bin/data/Newfilename.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\Users\Karina\Documents\of_v0.9.8_vs_release\apps\myApps\Ballerup_travel\bin"
-SumatraPDF.exe -print-to-default \data\boardingPasses\Paris.png
--- bin/filename.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd 'C:Program FilesSumatraPDF'
-SumatraPDF.exe -print-to-default data\SumatraPDF.exe -print-to-default C:\Users\Karina\Documents\of_v0.9.8_vs_release\apps\myApps\Ballerup_travel\bin/data/boardingPasses/Paris.png
